facaf79c5a27a5b59d34cefb1c31c619de125c24
Fraol-X <pfresh641@gmail.com>
2024-07-19 11:16:31 +0300

    add 'add' to className

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const removeButton = document.createElement('button');
     removeButton.textContent = 'Remove';
-    removeButton.className = 'remove-btn';
+    removeButton.className.add = 'remove-btn';
 
     removeButton.onclick = function() {
         taskList.removeChild(li);
